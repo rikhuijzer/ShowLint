687ffce07ac887148b93a912338258e59fd32660
rikhuijzer <rikhuijzer@users.noreply.github.com>
2020-12-17 11:00:50 +0000

    deploy: d163852a2abd93ad16233f1d86519fc29a9c4a62

--- assets/repositories/code/repos.jl
+++ assets/repositories/code/repos.jl
@@ -2,11 +2,11 @@
 # hideall
 using ShowLint
 
-for repo in repositories()
+for repo in repositories
   diff = ShowLint.apply("p2", repo)
   println("""
     ~~~
-    <h3>$repo</h3>
+    <h3>$(repo.name)</h3>
     p2.toml
     ~~~
     $diff

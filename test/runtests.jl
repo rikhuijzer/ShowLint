3b1a4af343052d4074a1ac8e85297a6da090d969
rikhuijzer <rikhuijzer@users.noreply.github.com>
2020-12-19 20:25:15 +0000

    deploy: d46eb6da4b986c1fbbdbd976e89050fbf41a6430

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,5 +4,10 @@ using Test
 SL = ShowLint
 
 @testset "ShowLint" begin
+    names = [r.name for r in SL.repositories]
+    sorted = Base.sort(names)
+    unique_and_sorted = unique(sorted)
+    @test names == unique_and_sorted
+    
     include("patterns.jl")
 end

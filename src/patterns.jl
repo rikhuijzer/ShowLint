e900fcf6e6f9e575c357cde6ee6f59d92d2ff45f
Rik <rikhuijzer@pm.me>
2020-12-20 16:43:27 +0100

    Improve p5, and add p6 and p7

--- src/patterns.jl
+++ src/patterns.jl
@@ -6,7 +6,7 @@ struct Pattern
     toml::String
 end
 
-const val_rx = raw"[\w_\[\]:\.()]*"
+const val_rx = raw"[\w_\[\]:\.]*"
 const fn_rx = raw"[\w_\.]*"
 
 patterns = [
@@ -61,7 +61,19 @@ patterns = [
         rewrite=':[bool]'
         """
     ),
-    Pattern(6, "Avoid findfirst in conditional", ["julia"],
+    Pattern(6, "Omit f(x) == f(x) and f(x) != f(x)", ["julia"],
+        "An extension of p5.",
+        """
+        match=':[[f]](:[x]) :[bool~(=|!)]= :[[f]](:[x])'
+
+        rule='where
+            rewrite :[bool] { "=" -> "true" },
+            rewrite :[bool] { "!" -> "false" }'
+
+        rewrite=':[bool]'
+        """
+    ),
+    Pattern(7, "Avoid findfirst in conditional", ["julia"],
         """
         For example, instead of `findfirst('a', \"ab\") === nothing`,
         use `occursin('a', \"ab\")` or `contains` (Julia ≥1.5).
